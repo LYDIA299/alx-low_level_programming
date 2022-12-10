c899ced55deac5b0267fd1996725be8f0c40b34b
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2022-12-10 16:15:07 +0100

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+putchar(n);
+putchar('\n');
+
+return (0);
+}
