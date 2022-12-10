6a8bbecdca9b9b861c175e6ad77c49c8e5062bda
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2022-12-10 10:58:25 +0100

    The last digit

--- 0x00-hello_world/1-last_digit.c
+++ 0x00-hello_world/1-last_digit.c
@@ -2,6 +2,7 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
+#include <string.h>
 /* betty style doc for function main goes there */
 /**
  *main - Entry point
@@ -18,12 +19,12 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 y = n % 10;
-printf("Last digit of %d is %d ", n, y);
+
 if (y > 5)
-printf("and is greater than 5\n");
+printf("Last digit of %d is %d and is greater than 5\n", n, y);
 else if (y == 0)
-printf("and is 0\n");
+printf("Last digit of %d is %d and is 0\n", n, y);
 else if (y < 6)
-printf("and is less than 6 and not 0\n");
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
 return (0);
 }
