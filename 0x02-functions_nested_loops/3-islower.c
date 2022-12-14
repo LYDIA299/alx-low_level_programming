3b2e9345f187f99c4829a7e6064189d65829254b
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2022-12-14 12:25:01 +0100

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_islower - checks if the argument is lowercase or not
+ *@c: is the parament
+ *Description: the unsigned char gets the caracter form of any 
+ *value/char passed and it's what we use to compare
+ *
+ *Return: 1 (success) and 0 (fail)
+ */
+
+int _islower(int c)
+{
+unsigned char i;
+char d;
+i = c;
+d = 'a';
+while (d <= 'z')
+{
+if (i == d)
+return (1);
+d++;
+}
+return (0);
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <ctype.h>
 
 /*prototypes*/
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
+int _islower(int c);
