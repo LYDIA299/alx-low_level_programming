98b4b02e4b845154b14fd16c474b259e013fa99e
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2022-12-14 22:20:17 +0100

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add - adds two integers
+ *Description: it takes in two arguments x and y
+ *@x: first parameter
+ *@y: second parameter
+ *Return: addition of two numbers
+ */
+
+int add(int x, int y)
+{
+int m = x + y;
+return (m);
+}
