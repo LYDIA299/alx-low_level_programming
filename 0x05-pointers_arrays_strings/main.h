4fe561e2c4c5d77f4ddc99864b973678e038846b
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2022-12-20 23:09:01 +0100

    prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 /*prototypes*/
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
