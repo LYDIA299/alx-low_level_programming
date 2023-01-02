08ba2fabc96e282f37ff66f88b6d8467ecf36e65
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-01-02 21:38:46 +0100

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,11 +4,12 @@
  *_memcpy - copies memory area
  *@dest: place to put the copied memory
  *@src: place we are to copy from
+ *@n:number of bytes copied
  *Return: pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	memcpy(dest, src, n);
+	memcpy(dest, src, n * sizeof(char));
 	return (dest);
 }
