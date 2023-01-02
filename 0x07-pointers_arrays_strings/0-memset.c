9c1324a5aa2120c02cb6d11f7ef9338ba36262f0
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-01-02 21:10:53 +0100

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_memset - fills the memory with a constant byte
+ *@s: starting point
+ *@b: character to fill the memory with
+ *@n: number of space to be filled
+ *Return: a pointer to the memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	/*built in function to solve the problem*/
+	memset(s, b, n*sizeof(char));
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#include <string.h>
+#include <unistd.h>
+
+/*Prototypes*/
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
