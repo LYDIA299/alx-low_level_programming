dd6e810aa1ac39d80cc0cc05c88d71982046e5b6
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-01-05 02:34:11 +0100

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "main.h"
 
 /**
@@ -8,10 +9,14 @@
 
 int is_prime_number(int n)
 {
-	if (n == 2 || n == 3 || n == 5 || n == 7)
-		return (1);
-	else if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 ==0) || (n % 7 ==0) || n == 1)
+	int i;
+
+	if (n == 1)
 		return (0);
-	else
-		return (1);
+	for (i = 2; i < sqrt(n); i++)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+	return (1);
 }
