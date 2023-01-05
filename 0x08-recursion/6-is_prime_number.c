a2f6db3f57d6f091a5cec0f799f510e984dbe264
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-01-05 02:18:48 +0100

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,9 @@
 
 int is_prime_number(int n)
 {
-	if ((n % 2 == 0) || (n % 3 == 0) || n == 1)
+	if (n == 2 || n == 3 || n == 5 || n == 7)
+		return (1);
+	else if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 ==0) || (n % 7 ==0) || n == 1)
 		return (0);
 	else
 		return (1);
