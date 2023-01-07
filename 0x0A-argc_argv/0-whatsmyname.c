63ee6b750b9e5bf2f034af956f975365003a741c
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-01-07 19:03:47 +0100

    a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@argc: parameter
+ *@ergv: parameter
+ *Return: 0 always
+ */
+
+int main(int argc, char *argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+argc and argv in c
+argc is the number of elements or items in argv
+argv is an array of string entered through the commandline
