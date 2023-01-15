5d2414958fd07dadb263dbfbeece269d78bb1898
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-01-15 21:56:13 +0100

    hey

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,33 +1,37 @@
 #include "main.h"
 
 /**
- *str_concat - concatenates two strings
- *@s1: parameter
- *@s2: parameter
- *Return: pointer or null
+ * str_concat - concatenate two strings using malloc
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to concatinated string
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, c, d = 0;
-	char *ptr;
+	char *a;
+	int i, j, c, d;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	ptr = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
-	if (ptr == NULL)
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (a == NULL)
 		return (NULL);
 
-	for (c = 0; c < (i + j + 1); c++)
+	for (c = 0, d = 0; c < (i + j + 1); c++)
 	{
 		if (c < i)
-			ptr[c] = s1[c];
+			a[c] = s1[c];
 		else
-			ptr[c] = s2[d++];
+			a[c] = s2[d++];
 	}
-
-	return (ptr);
+	return (a);
 }
