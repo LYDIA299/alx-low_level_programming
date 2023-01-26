22ce7d9f6b1b78015220f6342c671b1dcb5941d0
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-01-26 01:04:23 +0100

    function that searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+/**
+ * int_index - searches for an integer
+ * @array: array to search from
+ * @size: size of an array
+ * @cmp: callback function
+ *
+ * Return: index to the first element that returns true
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+
+		}
+	}
+	return (-1);
+}
