a017c4016acfd74190b3392e596ec59986a0ae65
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-01-31 15:35:39 +0100

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,31 @@
 #include "dog.h"
 
 /**
+ * copy_str - copies a string
+ * @str: string to copy
+ * Return: returns a pointer
+ */
+
+char *copy_str(char *str)
+{
+	char *ptr;
+	int i, j;
+
+	if (str == NULL)
+		return;
+
+	for (i = 0; *str != '\0'; i++)
+		;
+	ptr = malloc(sizeof(char) * (i + 1));
+	if (ptr == NULL)
+		return;
+	for (j = 0; j < (i + 1); j++)
+		ptr[j] = str[j];
+	ptr[j] = '\0';
+	return (ptr);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: parameter
  * @age: parameter
@@ -11,14 +36,27 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *newName, *newOwner;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
+	newName = copy_str(name);
+	newOwner = copy_str(owner);
 
-	d->name = name;
+	if (newName == NULL || newOwner == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+
+	d->name = newName;
 	d->age = age;
-	d->owner = owner;
+	d->owner = newOwner;
 
 	return (d);
 }
