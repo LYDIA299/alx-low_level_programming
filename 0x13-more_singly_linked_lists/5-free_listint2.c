da1dcb078320681ab253aa6a22d24fd324de0e65
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-02-06 22:38:46 +0100

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
+	listint_t *current, *temp;
 
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		current = (*head)->next;
-		free(*head);
-		*head = current;
+		current = *head;
+		while (current != NULL)
+		{
+			temp = current->next;
+			free(current);
+			current = temp;
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
