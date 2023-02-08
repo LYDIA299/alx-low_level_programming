239d7ac51d7cc3d9dd775c3908d2ea6919571efd
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-02-08 19:36:21 +0100

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,6 +10,11 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (n == 0 && index <64)
+		return (0);
+	if (index > 63)
+		return (-1);
+
 	while (n >= 1)
 	{
 		if (i == index)
