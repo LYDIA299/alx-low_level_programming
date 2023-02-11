384dc68a9a47f3da0914ddf1343e686ec9d5ddc7
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-02-11 12:33:28 +0100

    unction that appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	nwr = write(fd, text_content, nletters);
 
 	if (nwr == -1)
-			return (-1);
+		return (-1);
 
 	close(fd);
 	return (1);
