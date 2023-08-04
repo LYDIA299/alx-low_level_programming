97881d3b5b5a7edc2874ad93d7e5c0f4d0cf7b12
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-08-04 12:52:35 +0100

    Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: head pointer
+ * Return:  the number of nodes in the list
+ *
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nb = 0;
+
+
+	while(h)
+	{
+		nb++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nb);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,3 @@
+## 0x17. C - Doubly linked lists
+A doubly linked list is a kind of linked list with a link to the previous node
+as well as a data point and the link to the next node in the list
