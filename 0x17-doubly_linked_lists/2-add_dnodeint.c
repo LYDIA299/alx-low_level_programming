595dceb64fdccb0a5d250a224b5d779b3c77b2cf
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-08-04 13:21:53 +0100

    corrected betty errors

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,7 +2,8 @@
 
 /**
  * add_dnodeint - adds a new node at the beginning of a list
- * @h: head pointer
+ * @head: head pointer
+ * @n: the data(integer) of the node
  * Return: the address of the new element, or NULL if it failed
  *
  */
